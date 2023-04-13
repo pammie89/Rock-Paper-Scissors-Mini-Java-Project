f94fa4849caab560aea537ec1e78a3b6b6746897
Pam Afaneh <pamela.afaneh@gmail.com>
2023-04-13 11:46:07 -0500

    Player.java upload

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,4 +1,34 @@
 public abstract class Player {
 
-    //computerplayer and humanplayer files will extend from this.
+    //These are the instance variables
+   private int points;
+   private String move;
+
+   //This is the constructor
+   public Player() {
+       points = 0;
+   }
+
+   //Below are the initial methods I believe the game will use.  This may change.
+
+    //This method will increment player points.
+    public void incrementPoints() {
+       points++;
+   }
+    //This method contains an argument with a value of move
+   public void setMove(String move) {
+       this.move = move;
+   }
+    //This method returns the current value of move.
+   public String getMove() {
+       return move;
+   }
+    //This method returns the value of points.
+   public int getPoints() {
+       return points;
+   }
+
+
+
+
 }
